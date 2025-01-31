992c86d08a5a2f69e3198e975ff7956548a41727
DevSecOpsWithAWS <choudareddy1234@gmail.com>
2025-01-31 10:05:22 +0530

    Terraform

---
+++ conditions/ec2.tf
@@ -0,0 +1,26 @@
+resource "aws_instance" "terraform" {
+  ami = var.ami
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  instance_type = var.environment == "prod" ? "t2.small" : var.instance_type
+  tags = var.ec2-tags
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  
+  ingress {
+    from_port       = var.ingress_from_port
+    to_port         = var.ingress_to_port
+    protocol        = "tcp"
+    cidr_blocks      = var.cidr_blocks
+  }
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks      = var.cidr_blocks
+  }
+
+  tags = var.sg_tags
+}
--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "terraform" {
-  ami = "ami-09c813fb71547fc4f"
+  ami                    = "ami-09c813fb71547fc4f"
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
-  instance_type = "t2.micro"
+  instance_type          = "t2.micro"
   tags = {
-    Name = "Terraform-demo"
+    Name    = "Terraform-demo"
     Purpose = "Terraform practice"
   }
 }
@@ -11,18 +11,18 @@ resource "aws_instance" "terraform" {
 resource "aws_security_group" "allow_tls" {
   name        = "allow_tls"
   description = "Allow TLS inbound traffic and all outbound traffic"
-  
+
   ingress {
-    from_port       = 22
-    to_port         = 22
-    protocol        = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
